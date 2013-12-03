9115e4fa280362db903ecae89316e2a767941ab1
calve <calvinh34@gmail.com>
2013-12-03 15:01:02 +0100

    Show untracked, changed and staged in prompt

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -39,24 +39,48 @@ git_extra_info () {
 
 function p_pwd () {
 #Need zsh-git-prompt
+    
     if [ -n "$__CURRENT_GIT_STATUS" ]; then #Si on est dans un repertoire git
+	#This variable will save the "current" background color
+	CURRENTCOLOR=""
 	#Get repo name
 	PARENTDIR="%{$BG[$ZSH_THEME_PWD_BG1]%}%{$FG[0]%}$(basename $(dirname $(git rev-parse --show-toplevel)))"
 	THISDIR="%{$BG[$ZSH_THEME_PWD_BG2]%}%{$fg[black]%}"
 	THISDIR+="$(basename $(git rev-parse --show-toplevel))$(arrow $ZSH_THEME_PWD_BG2 black)"
+	CURRENTCOLOR="black"
 
 	#Get a (branch) name and a (state) color for HEAD with git_super_status set by zsh-git-prompt
 	THISDIR+="%{$bg[black]%}$(git_super_status)"
+	
+	#Print how much files are untracked
+	if [ "$GIT_UNTRACKED" -ne "0" ]; then
+	    THISDIR+="$(arrow $CURRENTCOLOR $ZSH_THEME_GIT_UNTRACKED_BG)%{$BG[$ZSH_THEME_GIT_UNTRACKED_BG]%}$FG[$ZSH_THEME_GIT_UNTRACKED_FG]$GIT_UNTRACKED"
+	    CURRENTCOLOR="$ZSH_THEME_GIT_UNTRACKED_BG"
+	fi
+	#Print how much files changed
+	if [ "$GIT_CHANGED" -ne "0" ]; then
+	    THISDIR+="$(arrow $CURRENTCOLOR $ZSH_THEME_GIT_CHANGED_BG)%{$BG[$ZSH_THEME_GIT_CHANGED_BG]%}$FG[$ZSH_THEME_GIT_CHANGED_FG]$GIT_CHANGED"
+	    CURRENTCOLOR="$ZSH_THEME_GIT_CHANGED_BG"
+	fi
+	#Print how much commit
+	if [ "$GIT_STAGED" -ne "0" ]; then
+	    THISDIR+="$(arrow $CURRENTCOLOR $ZSH_THEME_GIT_STAGED_BG)%{$BG[$ZSH_THEME_GIT_STAGED_BG]%}$FG[$ZSH_THEME_GIT_STAGED_FG]$GIT_STAGED"
+	    CURRENTCOLOR="$ZSH_THEME_GIT_STAGED_BG"
+	fi
+      
 	if [[ -n $(git rev-parse --show-prefix 2>/dev/null) ]]; then #Si on est dans un sous répertoire
-	    THISDIR+="$(arrow black $ZSH_THEME_PWD_BG2)"
+	    THISDIR+="$(arrow $CURRENTCOLOR $ZSH_THEME_PWD_BG2)"
 	    THISDIR+="%{$BG[$ZSH_THEME_PWD_BG2]%}%{$fg[black]%}$(git rev-parse --show-prefix)"
 	    THISDIR+="$(arrow $ZSH_THEME_PWD_BG2 default)"
 	else
-	    THISDIR+="$(arrow black default)" 
+	    THISDIR+="$(arrow $CURRENTCOLOR default)" 
 	fi
+
     else
-	PARENTDIR="%{$BG[$ZSH_THEME_PWD_BG1]%}%{$FG[0]%}$(dirname $(print -P %3~))"
-	THISDIR="%{$BG[$ZSH_THEME_PWD_BG2]%}%{$fg[black]%}$(print -P %1~)$(arrow $ZSH_THEME_PWD_BG2 default)"
+	PARENTDIR="%{$BG[$ZSH_THEME_PWD_BG1]%}%{$FG[$ZSH_THEME_PWD_FG1]%}$(dirname $(print -P %3~))"
+	THISDIR="%{$BG[$ZSH_THEME_PWD_BG2]%}%{$FG[$ZSH_THEME_PWD_FG2]%}$(print -P %1~)$(arrow $ZSH_THEME_PWD_BG2 default)"
     fi
-	echo -n "$PARENTDIR$(arrow $ZSH_THEME_PWD_BG1 $ZSH_THEME_PWD_BG2 )$THISDIR"
+
+    #Finally, print everything
+    echo -n "$PARENTDIR$(arrow $ZSH_THEME_PWD_BG1 $ZSH_THEME_PWD_BG2 )$THISDIR"
 }
