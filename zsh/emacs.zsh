a17427bc00e2efd1310918581527dff5d9ac98ca
calve <calvinh34@gmail.com>
2014-02-01 18:29:28 +0100

    Human comprehension

--- zsh/emacs.zsh
+++ zsh/emacs.zsh
@@ -4,12 +4,13 @@
 
 export EDITOR='emacsclient -ta ""'
 
+alias e='emacsclient -ta ""'
+
 # This starts a new daemon as the user if it's not running already (-a "") and uses TRAMP so that there doesn't have to be another daemon for "root"
+# It is like 'sudo e'
 function E() { 
     emacsclient -t -a "" "/sudo::$@" 
 } 
 
-
-alias e='emacsclient -ta ""'
 
 compdef emacs
