211bd7cc7e85f5d111f7c4988f640365f23e0a81
calve <calvinh34@gmail.com>
2015-05-11 19:06:35 +0200

    Disable fortune

--- zsh/hello.zsh
+++ zsh/hello.zsh
@@ -3,7 +3,3 @@
 #################
 
 echo -e "Salut $fg[green]$USER$reset_color ! Bienvenue sur $fg[blue]$(uname -n)$reset_color :) "
-#echo -e "\e[1;32mfortune bashfr" 
-echo -en "\e[1;33m"
-fortune bashfr
-echo -e "\e[0m"
