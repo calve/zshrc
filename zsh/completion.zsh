b4591098d06f65078a2cf6cbde5d6ca652e070d7
calve <calvinh34@gmail.com>
2015-07-10 16:31:49 +0200

    Completion for openssl

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -13,5 +13,7 @@ zstyle ':completion:*' menu select=1
 zstyle ':completion:*' select-prompt '%SScrolling active: current selection at %p%s'
 zstyle :compinstall filename '/home/goudale/.zshrc'
 
+fpath=(~/.zsh/completions $fpath)
+
 #Completion des options 
 compdef _gnu_generic subdl subdownloader lm airodump-ng convert curl prof
