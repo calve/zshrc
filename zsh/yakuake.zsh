d3808b439c18f31191743a2805edca02275e9e4d
calve <calvinh34@gmail.com>
2016-11-25 16:19:45 +0100

    Remove yakuake blur

--- zsh/yakuake.zsh
+++
@@ -1,6 +0,0 @@
-#################
-#Yakuake blur
-#################
-if [[ -n $DISPLAY && -n `qdbus | grep yakuake` ]]; then
-    xprop -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 -name Yakuake;
-fi
